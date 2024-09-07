8c29c47ef3a907050b5cc37f8348a86626317d12
Slava Mokrynskyi <chempionslava@gmail.com>
2024-09-07 20:38:07 +0300

    Add project files.

---
+++ dz/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>;
+
+using namespace std;
+
+int main() {
+
+
+	cout << "\tKanye West - Gorgeuous"<<endl;
+	cout << "" << endl;
+	cout << "Ain't no question if I want it, I need it"<< endl;
+	cout << "I can feel it slowly drifting away from me "<< endl;
+	cout << "I'm on the edge, so why you playing? I'm saying "<< endl;
+	cout << "I will never ever let you live this down, down, down" << endl;
+	cout << "Not for nothing, I've foreseen it, I dreamed it"<< endl;
+	cout << "I can feel it slowly drifting away from me " << endl;
+	cout << "No more chances, if you blow this, you bogus "<< endl;
+	cout << "I will never ever let you live this down, down, down" << endl;
+	cout << "" << endl;
+
+	cout << "\033[31mEvery\n\033[38;5;208mhunter\n\033[33mwants\n\033[32mto know\n\033[36mwhere\n\033[34mthe pheasant\n\033[35mis\n\033[0m";
+	cout << "" << endl;
+
+	cout << "Sell pupies\nBreed: Rottweiler\n...\nPhone-num: 222-22-22\n222\t| 222 |\n22\t|  22 |\n22\t|  22 |";
+
+}
